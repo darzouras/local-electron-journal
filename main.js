da33b6e8f27d94ed7ec0398c0df9f97f1c8834a7
Darlene Zouras <darZouras@Darlenes-MacBook-Pro.local>
2016-12-05 21:09:12 -0600

    Building up handlebars views and layouts

--- main.js
+++ main.js
@@ -9,15 +9,16 @@ const BrowserWindow = electron.BrowserWindow;
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
+
+// ESSENTIAL ELCTRON STUFF!!!
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 600, height: 400});
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   // Open the DevTools.
-  //mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -49,6 +50,3 @@ app.on('activate', function () {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
